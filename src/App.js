c608a7ff7d22c9f4a98f76abb32b0f824b6d5eb9
jjiang316 <jjiang316@gmail.com>
2022-05-02 18:21:04 -0500

    add images to result page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import logo from './logo.svg';
+import kurimu from './kurimu.png';
+import tamale from './eveliasTamales.png';
+import bakery from './lostLarsonBakery.png';
 import './App.css';
 import CoffeeIcon from '@mui/icons-material/Coffee';
 import ColorLensIcon from '@mui/icons-material/ColorLens';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import RestaurantIcon from '@mui/icons-material/Restaurant';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem'; 
+import Grid from '@mui/material/Grid';
+
+const itemData= [{
+  img: kurimu
+},
+{
+  img: tamale
+},
+{
+  img: bakery
+}];
 
 function App() {
   return (
@@ -35,13 +50,33 @@ function App() {
 
           </div>
 
-        <div className='reccomendations'>
-        <h1> Unfound reccomendations for <RestaurantIcon/> </h1>
+        <div className='recomendations'>
+        <h1> Unfound recomendations for <RestaurantIcon/> </h1>
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+        <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+          {itemData.map((item) => (
+            <ImageListItem key={item.img}>
+              <img
+                src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.title}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList></Grid>
+
         </div>
-        {/* <div className='reccomendationPhotos' >
+        {/* <div className='recomendationPhotos' >
 
         </div> */}
-
+  
         
 
     </div>
