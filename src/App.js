977a9b707d3a11adb39c8463a719247f043957d4
joshBreite <joshuabreite2022@u.northwestern.edu>
2022-05-06 15:39:09 -0500

    Small Data on Quiz Structure -- Josh (Individual)

--- src/App.js
+++ src/App.js
@@ -14,6 +14,30 @@ const itemData = [{
   img: bakery
 }];
 
+const quiz = {
+  "I feel most energetic and focused:": 
+    [
+      "In the morning",
+      "During the afternoon and early evening",
+      "Late at night"
+    ],
+  "When I walk, I tend to do it:":
+    [
+      "Fairly fast, with long steps",
+      "Fairly fast, with small steps",
+      "Less fast, head up, looking the world in the face",
+      "Less fast, head down",
+      "Very slowly"
+    ],
+    "When you speak to people, you tend to:":
+    [
+      "Your knees bent, with your legs neatly side by side",
+      "Your legs crossed",
+      "Your legs stretched out or straight",
+      "One leg curled under you"
+    ]
+}
+
 function App() {
   return (
     <>
