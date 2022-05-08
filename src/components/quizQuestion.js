c7699c4f1e710046562e1fb68ab327f6b9904e13
aavi-deora <aavideora2022@u.northwestern.edu>
2022-05-07 23:52:16 -0500

    fixed issue: answer selection
    Now, users can only chose one answer

--- src/components/quizQuestion.js
+++ src/components/quizQuestion.js
@@ -15,47 +15,23 @@ function QuizQuestion ({question, answers}) {
             {question[0]}
         </Typography>
         {
-            Object.values(question[1]).map(answer => <QuizAnswer answer={answer}/>)
+            QuizAnswer (question[1])
+            // Object.values(question[1]).map(answer => <QuizAnswer answer={answer}/>)
         }
         </CardContent>
         </Card>
     )
 }
-// function QuizAnswer ({answer}) {
-//     console.log({answer});
 
-//     const [value, setValue] = useState();
-
-//     const handleChange = (event) => {
-//         setValue(event.target.value);
-//       };
-    
-
-//     return(
-//     <FormControl >
-//         <RadioGroup
-//             aria-labelledby="demo-controlled-radio-buttons-group"
-//             name="controlled-radio-buttons-group"
-//             value={value}
-//             onChange={handleChange}
-//         >
-//             <FormControlLabel id="demo-radio-buttons-group-label" value={answer} control={<Radio />} label={answer} />
-
-//         </RadioGroup>
-//     </FormControl>
-//     )
-
-// }
-
-function QuizAnswer ({answer}) {
-    console.log({answer});
+function QuizAnswer (answers) {
 
     const [value, setValue] = useState();
 
     const handleChange = (event) => {
         setValue(event.target.value);
       };
-    
+
+
 
     return(
     <FormControl >
@@ -65,16 +41,16 @@ function QuizAnswer ({answer}) {
             value={value}
             onChange={handleChange}
         >
-            {/* {
-                Object.values(answer).map((ans) => <FormControlLabel id="demo-radio-buttons-group-label" value={ans} control={<Radio />} label={ans} /> )
-            } */}
-            <FormControlLabel id="demo-radio-buttons-group-label" value={answer} control={<Radio />} label={answer} />
-
+            {
+                Object.values(answers).map((ans) => <FormControlLabel id="demo-radio-buttons-group-label" value={ans} control={<Radio />} label={ans} /> )
+            }
         </RadioGroup>
     </FormControl>
     )
 
 }
+
+
 
 function QuizQuestionList({quiz}) {
 
