edd3cc5364a6710f3f7d97e7277cfaa4b84a7629
jjiang316 <jjiang316@gmail.com>
2022-05-26 15:49:41 -0500

    remove unused lines

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
-import kurimu from './kurimu.png';
-import tamale from './eveliasTamales.png';
-import bakery from './lostLarsonBakery.png';
 import './App.css';
 import { Button, Typography } from '@mui/material';
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-const itemData = [{
-  img: kurimu
-},
-{
-  img: tamale
-},
-{
-  img: bakery
-}];
+
 
 const myTheme = createTheme({
   typography: {
