95b7068b0765f8d109cc48884b446099c2d52b56
miia12 <805417874@qq.com>
2022-06-02 11:11:11 -0500

    remove unused import&class

--- src/components/quizQuestion.js
+++ src/components/quizQuestion.js
@@ -8,9 +8,6 @@ import { getSuggestions } from '../routes/question';
 import { Button } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import { sizing } from '@mui/system';
-import { Padding } from "@mui/icons-material";
 
 
 
